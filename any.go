457d3cfc7ed24601c368744ea867bfc89fc1d960
HAYAMA_Kaoru <kaoru.hayama@alfatech.jp>
2022-03-18 11:34:50 +0900

    Remove unused types

--- any.go
+++
@@ -1,6 +0,0 @@
-package lazy
-
-//go:generate go run github.com/cheekybits/genny -in=main.go -out=zmain.go gen "Item=string,Bytes,bool,int,int64,uint,uint64,uintptr,float64,Any"
-
-type Any = interface{}
-type Bytes = []byte
