0dc49ce75eea683026c1b0143dc1c714d235bd88
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-03-01 18:38:50 +0900

    Implement the structure `Two` like `"sync".OnceValues`

--- main.go
+++ main.go
@@ -23,3 +23,20 @@ func (this *Of[T]) Value() T {
 func New[T any](newfunc func() T) *Of[T] {
 	return &Of[T]{New: newfunc}
 }
+
+type Two[T any, E any] struct {
+	New    func() (T, E)
+	once   sync.Once
+	value1 T
+	value2 E
+}
+
+func (this *Two[T, E]) Values() (T, E) {
+	if this.New != nil {
+		this.once.Do(func() {
+			this.value1, this.value2 = this.New()
+			this.New = nil
+		})
+	}
+	return this.value1, this.value2
+}
